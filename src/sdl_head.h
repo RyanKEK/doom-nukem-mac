1754c9a82099298c0ca94cdc01c444105440dc77
RyanKEK <californiahoteltv@gmail.com>
2019-08-06 18:50:54 +0300

    ceiling and floor textures

--- src/sdl_head.h
+++ src/sdl_head.h
@@ -10,6 +10,23 @@
 #define H 1000
 #define OK 1
 #define ERROR 0
+#define min(a,b)             (((a) < (b)) ? (a) : (b)) // min: Choose smaller of two scalars.
+#define max(a,b)             (((a) > (b)) ? (a) : (b)) // max: Choose greater of two scalars.
+#define clamp(a, mi,ma)      min(max(a,mi),ma)
+#define vxs(x0,y0, x1,y1)    ((x0)*(y1) - (x1)*(y0))   // vxs: Vector cross product
+// Overlap:  Determine whether the two number ranges overlap.
+#define Overlap(a0,a1,b0,b1) (min(a0,a1) <= max(b0,b1) && min(b0,b1) <= max(a0,a1))
+// IntersectBox: Determine whether two 2D-boxes intersect.
+#define IntersectBox(x0,y0, x1,y1, x2,y2, x3,y3) (Overlap(x0,x1,x2,x3) && Overlap(y0,y1,y2,y3))
+// PointSide: Determine which side of a line the point is on. Return value: <0, =0 or >0.
+#define PointSide(px,py, x0,y0, x1,y1) vxs((x1)-(x0), (y1)-(y0), (px)-(x0), (py)-(y0))
+// Intersect: Calculate the point of intersection between two lines.
+#define Intersect(x1,y1, x2,y2, x3,y3, x4,y4) ((struct vertex) { \
+    vxs(vxs(x1,y1, x2,y2), (x1)-(x2), vxs(x3,y3, x4,y4), (x3)-(x4)) / vxs((x1)-(x2), (y1)-(y2), (x3)-(x4), (y3)-(y4)), \
+    vxs(vxs(x1,y1, x2,y2), (y1)-(y2), vxs(x3,y3, x4,y4), (y3)-(y4)) / vxs((x1)-(x2), (y1)-(y2), (x3)-(x4), (y3)-(y4)) })
+#define dtor(a)         (a * 0.0174533)
+#define rtod(a)         (a * 57.2958)
+#define distance(x1, y1 ,x2, y2) (sqrtf((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)))
 
 typedef struct		s_point
 {
@@ -24,6 +41,8 @@ typedef	struct		s_sdl
 	SDL_Window		*win;
 	SDL_Renderer	*ren;
 }					t_sdl;
+
+struct Scaler { int result, bop, fd, ca, cache; };
 
 //					CREATE
 t_sdl				*new_t_sdl(int win_size_x, int win_size_y, const char *title);
@@ -41,8 +60,11 @@ SDL_Surface			*load_img(const char *file_name);
 
 //					TEXTURE
 SDL_Texture			*load_texture(const char *file_name, SDL_Renderer *ren);
+void    draw_texture_line(SDL_Surface *screen, SDL_Surface *image, int screenX, int xStart, int xEnd, int top, int bottom);
 SDL_Texture			*texture_from_surf(SDL_Surface *surf, SDL_Renderer *ren);
 SDL_Texture			*new_fresh_texture(SDL_Renderer *ren, int width, int height);
+void textLine(int x, int y1,int y2, struct Scaler ty,unsigned txtx, SDL_Surface *surface, SDL_Surface *image);
+Uint32              getpixel(SDL_Surface *surface, int x, int y);
 
 //					RENDER
 void				sdl_render(SDL_Renderer *ren, SDL_Texture *tex, SDL_Rect *src, SDL_Rect *dst);
