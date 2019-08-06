2971b12aee4cf3627116fef8521b595d67f60042
RyanKEK <californiahoteltv@gmail.com>
2019-08-06 18:47:36 +0300

    ceiling and floor textures

---
+++ src/textures.c
@@ -0,0 +1,77 @@
+#include "sdl_head.h"
+
+void    draw_texture_line(SDL_Surface *screen, SDL_Surface *image, int screenX, int xStart, int xEnd, int top, int bottom)
+{
+    int *screenPix = (int*)screen->pixels;
+    int *imagePix = (int*)image->pixels;
+    float lineLength = abs(top - bottom);
+    if (xEnd - xStart == 0)
+        return ;
+    int x = (float)(screenX - xStart) / (float)(xEnd - xStart) * (float)image->w;
+    float y = 0;
+    if (top < 0)
+        y = (double)image->h / lineLength * abs(top);
+    top = clamp(top, 0, H - 1);
+    bottom = clamp(bottom, 0, H - 1);
+    while (top < bottom)
+    {
+        //if (top >= 0)
+        screenPix[(int)(screenX + top * screen->w)] = imagePix[x + (int)y * image->w];
+        top++;
+        if (lineLength != 0)
+            y += (double)image->h / lineLength;
+    }
+}
+
+Uint32 getpixel(SDL_Surface *surface, int x, int y)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch(bpp) {
+        case 1:
+            return *p;
+            break;
+
+        case 2:
+            return *(Uint16 *)p;
+            break;
+
+        case 3:
+            if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+                return p[0] << 16 | p[1] << 8 | p[2];
+            else
+                return p[0] | p[1] << 8 | p[2] << 16;
+            break;
+
+        case 4:
+            return *(Uint32 *)p;
+            break;
+
+        default:
+            return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
+
+static int Scaler_Next(struct Scaler* i)
+{
+    for(i->cache += i->fd; i->cache >= i->ca; i->cache -= i->ca) i->result += i->bop;
+    return i->result;
+}
+
+void textLine(int x, int y1,int y2, struct Scaler ty,unsigned txtx, SDL_Surface *surface, SDL_Surface *image)
+{
+    int *pix = (int*) surface->pixels;
+    int *imagePix = (int*)image->pixels;
+    y1 = clamp(y1, 0, H-1);
+    y2 = clamp(y2, 0, H-1);
+    pix += y1 * W + x;
+    for(int y = y1; y <= y2; ++y)
+    {
+        unsigned txty = Scaler_Next(&ty);
+        //*pix = imagePix[txtx % image->w + (txty % image->h) * image->w];
+        *pix = getpixel(image, txtx % image->w, txty % image->h);
+        pix += W;
+    }
+}
